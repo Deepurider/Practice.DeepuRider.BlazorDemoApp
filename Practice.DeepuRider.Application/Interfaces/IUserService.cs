529cb04c797d9c15d9bee28c2d531b3ed56e13c7
DEEP <PATELDEEP691996@GMAIL.COM>
2025-12-07 15:03:33 +0530

    Initial commit to add project and other required stuff.

---
+++ Practice.DeepuRider.Application/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Practice.DeepuRider.Domain.Models;
+
+namespace Practice.DeepuRider.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User?> GetAsync(int userId, CancellationToken cancellationToken);
+        Task<IQueryable<User>> GetListAsync(CancellationToken cancellationToken);
+        Task<User> Create(User user, CancellationToken cancellationToken);
+    }
+}
---
+++ Practice.DeepuRider.Application/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practice.DeepuRider.Application.Interfaces;
+using Practice.DeepuRider.Domain.Data;
+using Practice.DeepuRider.Domain.Models;
+
+namespace Practice.DeepuRider.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly BlazorDomainContext _context;
+
+        public UserService(BlazorDomainContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Create(User user, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(user, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+            return user;
+        }
+
+        public async Task<User?> GetAsync(int userId, CancellationToken cancellationToken)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.UserId == userId, cancellationToken);
+        }
+
+        public async Task<IQueryable<User>> GetListAsync(CancellationToken cancellationToken)
+        {
+            return _context.Users.AsQueryable();
+        }
+    }
+}
